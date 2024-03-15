aeaf64afc6b81792a42aa0a64e98a23fefab4b16
NguyenHao <nguyenhaocu0@gmail.com>
2024-03-15 09:22:18 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,28 +1,50 @@
-
+/* eslint-disable no-console */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, CLOSE_DB, } from '~/config/mongodb'
+import { env } from '~/config/environment'
 
-const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+const START_SERVER = () => {
+  const app = express()
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  const hostname = 'localhost'
+  const port = 8017
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`I am running at ${ hostname }:${ port }/`)
-})
+  app.get('/', async (req, res) => {
+    // console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
+
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`I am ${env.AUTHOR} running at ${ env.APP_HOST }:${ env.APP_PORT }`)
+
+    exitHook(() => {
+      console.log('App closed')
+      CLOSE_DB()
+    })
+  })
+}
+
+(async () => {
+  try {
+    console.log('connecting to MongoDB Cloud Atlas')
+    await CONNECT_DB()
+    console.log('connected to MongoDB Cloud Atlas')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+
+// CONNECT_DB()
+//   .then( () => console.log('connected to MongoDB Cloud Atlas'))
+//   .then( () => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
+
+
