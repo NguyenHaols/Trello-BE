a0645fd9f44ee8c5f14ec3e4c53599f837798e7e
Nguyễn Đình Hào <91510339+NguyenHaols@users.noreply.github.com>
2024-07-17 14:27:58 +0700

    deploy2

--- src/server.js
+++ src/server.js
@@ -2,12 +2,12 @@
 
 import express from 'express'
 import exitHook from 'async-exit-hook'
-import { CONNECT_DB, CLOSE_DB, } from '~/config/mongodb'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1'
 import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware'
 import cors from 'cors'
-import { corsOptions } from './config/cors' 
+import { corsOptions } from './config/cors'
 import cookieParser from 'cookie-parser'
 
 const START_SERVER = () => {
@@ -32,7 +32,7 @@ const START_SERVER = () => {
   if (env.BUILD_MODE === 'production') {
     app.listen(process.env.PORT, () => {
       console.log(`I am ${env.AUTHOR} running at :${ process.env.PORT } in production`)
-  
+
       exitHook(() => {
         console.log('App closed')
         CLOSE_DB()
@@ -41,14 +41,14 @@ const START_SERVER = () => {
   } else {
     app.listen(env.APP_PORT, env.APP_HOST, () => {
       console.log(`I am ${env.AUTHOR} running at ${ env.APP_HOST }:${ env.APP_PORT }`)
-  
+
       exitHook(() => {
         console.log('App closed')
         CLOSE_DB()
       })
     })
   }
-  
+
 }
 
 (async () => {
