d1110687282b0954741888c24eebab7ab6f1d968
Nguyễn Đình Hào <91510339+NguyenHaols@users.noreply.github.com>
2024-12-16 10:47:23 +0700

    update to deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 //
 export const WHITELIST_DOMAINS = [
   'http://localhost:5173',
-  'https://trello-project-tau.vercel.app'
+  'https://trello-project-tau.vercel.app',
+  'https://trello-be.vercel.app'
 ]
